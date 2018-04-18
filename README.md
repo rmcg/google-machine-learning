0e6829bfeaaba17331c47a85822ecb9db73dd7fe
ryan-mcgivern-hs <ryan.mcgivern@hootsuite.com>
2018-04-18 17:12:20 -0400

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Gamma Cohort Boilerplate #
+
+## Artifactory ##
+Comment out hootsuite artifactorylines in ~/.pip/pip.config so you can install external dependencies
+
+## Install Requirements ##
+`pip install -r requirements.txt`
+
+## Run examples ##
+`python validation.py`
---
+++ validation/validation.py
@@ -0,0 +1,93 @@
+#####################
+# Snippet 1
+#####################
+import math
+
+from IPython import display
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+import tensorflow as tf
+from tensorflow.python.data import Dataset
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+pd.options.display.max_rows = 10
+pd.options.display.float_format = '{:.1f}'.format
+
+california_housing_dataframe = pd.read_csv("https://storage.googleapis.com/mledu-datasets/california_housing_train.csv", sep=",")
+
+#####################
+# Snippet 2
+#####################
+def preprocess_features(california_housing_dataframe):
+  """Prepares input features from California housing data set.
+
+  Args:
+    california_housing_dataframe: A Pandas DataFrame expected to contain data
+      from the California housing data set.
+  Returns:
+    A DataFrame that contains the features to be used for the model, including
+    synthetic features.
+  """
+  selected_features = california_housing_dataframe[
+    ["latitude",
+     "longitude",
+     "housing_median_age",
+     "total_rooms",
+     "total_bedrooms",
+     "population",
+     "households",
+     "median_income"]]
+  processed_features = selected_features.copy()
+  # Create a synthetic feature.
+  processed_features["rooms_per_person"] = (
+    california_housing_dataframe["total_rooms"] /
+    california_housing_dataframe["population"])
+  return processed_features
+
+def preprocess_targets(california_housing_dataframe):
+  """Prepares target features (i.e., labels) from California housing data set.
+
+  Args:
+    california_housing_dataframe: A Pandas DataFrame expected to contain data
+      from the California housing data set.
+  Returns:
+    A DataFrame that contains the target feature.
+  """
+  output_targets = pd.DataFrame()
+  # Scale the target to be in units of thousands of dollars.
+  output_targets["median_house_value"] = (
+    california_housing_dataframe["median_house_value"] / 1000.0)
+  return output_targets
+
+#####################
+# Snippet 3
+#####################
+training_examples = preprocess_features(california_housing_dataframe.head(12000))
+print training_examples.describe()
+
+#####################
+# Snippet 3
+#####################
+training_targets = preprocess_targets(california_housing_dataframe.head(12000))
+print training_targets.describe()
+
+#####################
+# Snippet 4
+#####################
+validation_examples = preprocess_features(california_housing_dataframe.tail(5000))
+print validation_examples.describe()
+
+#####################
+# Snippet 5
+#####################
+validation_targets = preprocess_targets(california_housing_dataframe.tail(5000))
+print validation_targets.describe()
+
+#####################
+# Programming Task
+#####################
+# TODO
